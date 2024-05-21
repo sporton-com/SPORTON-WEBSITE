44b343dbe3dcc54e2c96f07033b61bd91ed9b7fe
Ahmed <ahmed15ayman7ahmed2002@gmail.com>
2024-05-22 01:11:34 +0300

    descripton

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,8 +5,41 @@ import '../globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Sporton',
-  description: 'Rero registration information in application',
+  title: 'SPORTEN',
+  keywords:[
+    "player CV",
+    "sports opportunities",
+    "sports clubs",
+    "player profiles",
+    "showcase achievements",
+    "connect with clubs",
+    "sports talent",
+    "sports recognition",
+    "athlete portfolio",
+    "sports scouting",
+    "sports networking",
+    "talent showcase",
+    "sports recruitment",
+    "athlete profiles",
+    "sports career",
+    "sports management",
+    "talent discovery",
+    "sports networking platform",
+    "athletic achievements",
+    "talent identification",
+    "sports resume",
+    "athlete connections",
+    "sports community",
+    "talent promotion",
+    "sports talent search",
+    "athlete recruitment",
+    "sports showcase",
+    "talent evaluation",
+    "sports talent pool",
+    "athlete opportunities",
+    "sports networking site"
+  ],
+  description: 'SPORTEN provides an opportunity for talented individuals who struggle to get into clubs and show their abilities. Highlight your skills and achievements on SPORTEN and attract everyone\'s attention',
   icons:'/logo.svg'
 }
 

a112426cd509e8ebc712031fbe712ff9e65bb3f0
Ahmed <ahmed15ayman7ahmed2002@gmail.com>
2024-07-21 14:36:06 +0300

    update chat notify

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { useState, useEffect } from 'react';
+"use client";
+import { useState, useEffect } from "react";
 import { fetchAllUser, fetchUser } from "@/lib/actions/user.actions";
 import { SugCard } from "../cards/sugCard";
 
@@ -11,11 +11,31 @@ interface Props {
   setChat?: any;
 }
 
-const RightSidebar: React.FC<Props> = ({ isChat, Ids, isxl,islg,setChat }) => {
+const RightSidebar: React.FC<Props> = ({
+  isChat,
+  Ids,
+  isxl,
+  islg,
+  setChat,
+}) => {
   const [userInfo, setUserInfo] = useState<any>(null);
   const [users, setUsers] = useState<any>(null);
   const [refrish, setrefrish] = useState<any>(null);
 
+  useEffect(() => {
+    if (
+      Notification.permission !== "granted" &&
+      Notification.permission !== "denied"
+    ) {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          console.log("Notification permission granted.");
+        } else {
+          console.log("Notification permission denied.");
+        }
+      });
+    }
+  }, []);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -35,11 +55,26 @@ const RightSidebar: React.FC<Props> = ({ isChat, Ids, isxl,islg,setChat }) => {
     fetchData();
   }, [refrish]);
   return (
-    <section className={`rightsidebar custom-scrollbar  ${isxl ? "w-full" : "max-xl:hidden"}  ${isChat ? ` p-0 ${!Ids && " px-1"} w-96` : "p-3 pt-28 w-72"}`}>
+    <section
+      className={`rightsidebar custom-scrollbar  ${
+        isxl ? "w-full" : "max-xl:hidden"
+      }  ${isChat ? ` p-0 ${!Ids && " px-1"} w-96` : "p-3 pt-28 w-72"}`}>
       <div className="flex flex-1 flex-col justify-start">
-        {!isChat && <h1 className=" text-heading4-medium text-light-1 mb-6">Player</h1>}
-        {users && users.users && userInfo &&
-          <SugCard result2={JSON.stringify(users.users)} userInfo2={JSON.stringify(userInfo)} type={"users"} isChat={isChat} Ids={Ids ? Ids : ''} islg={islg} refrish={setrefrish} setChat={setChat} />}
+        {!isChat && (
+          <h1 className=" text-heading4-medium text-light-1 mb-6">Player</h1>
+        )}
+        {users && users.users && userInfo && (
+          <SugCard
+            result2={JSON.stringify(users.users)}
+            userInfo2={JSON.stringify(userInfo)}
+            type={"users"}
+            isChat={isChat}
+            Ids={Ids ? Ids : ""}
+            islg={islg}
+            refrish={setrefrish}
+            setChat={setChat}
+          />
+        )}
       </div>
     </section>
   );

11bef963ab46d70dd4a82810e34dcd738432b24c
Ahmed <ahmed15ayman7ahmed2002@gmail.com>
2024-07-23 12:48:16 +0300

    update errors

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,80 +1,70 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
-import '../globals.css'
-import Topbar from '@/components/shared/Topbar';
-import BottomSidebar from '@/components/shared/Bottombar';
-import LeftSidebar from '@/components/shared/LeftSidebar';
-import RightSidebar from '@/components/shared/RightSidebar';
-// import { GoogleTagManager } from '@next/third-parties/google';
-// import { ThemeProvider } from '@/components/providers/theme-provider';
+import "../globals.css";
+import Topbar from "@/components/shared/Topbar";
+import BottomSidebar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    default: "SPORTON | Home - Player Posts, Achievements, and Contact Players ",
+    default:
+      "SPORTON | Home - Player Posts, Achievements, and Contact Players ",
     template: "%s - SPORTON ",
   },
-  keywords:[
+  keywords: [
+    "SPORTON",
+    "Home",
+    "Player Posts",
+    "Achievements",
+    "Contact Players",
 
-      "SPORTON",
-      "Home",
-      "Player Posts",
-      "Achievements",
-      "Contact Players",
-
-      "Showcase Talent",
-      "Connect with Clubs",
-      "Gain Recognition",
-      "Sports Platform",
-    ],
-  description: 'SPORTON is a sports community platform that includes all athletes from Egypt in various sports, and on the other hand, player agents and clubs and institutions that will receive their talents will be present.',
+    "Showcase Talent",
+    "Connect with Clubs",
+    "Gain Recognition",
+    "Sports Platform",
+  ],
+  description:
+    "SPORTON is a sports community platform that includes all athletes from Egypt in various sports, and on the other hand, player agents and clubs and institutions that will receive their talents will be present.",
   openGraph: {
-    type: 'website',
-    url: 'https://www.sporton.website/',
-    title: 'SPORTON',
-    description: 'SPORTON is a sports community platform that includes all athletes from Egypt in various sports, and on the other hand, player agents and clubs and institutions that will receive their talents will be present.',
+    type: "website",
+    url: "https://www.sporton.website/",
+    title: "SPORTON",
+    description:
+      "SPORTON is a sports community platform that includes all athletes from Egypt in various sports, and on the other hand, player agents and clubs and institutions that will receive their talents will be present.",
     images: [
       {
-        url: 'https://www.sporton.website/logo.png',
-        alt: 'SPORTON LOGO',
+        url: "https://www.sporton.website/logo.png",
+        alt: "SPORTON LOGO",
       },
     ],
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-    <html lang="en"  >
-      <body className={inter.className}>
-
-      
-      <Topbar/>
-      <main className=' flex flex-row w-full'>
-
-        <LeftSidebar/>
-        <section className="main-container">
-        <div className=" w-full max-w-4xl">
-        {children}
-        </div>
-        </section>
-        {/* @ts-ignore */}
-        <RightSidebar/>
-      </main>
-      <BottomSidebar/>
-      {/* <GoogleTagManager gtmId="GTM-WB4S5V8V" /> */}
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+          <main className=" flex flex-row w-full">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className=" w-full max-w-4xl">{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
+          <BottomSidebar />
         </body>
-    </html>
-
-          
-   </ClerkProvider>
-  )
+      </html>
+    </ClerkProvider>
+  );
 }

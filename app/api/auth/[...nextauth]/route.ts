eebcc64c592443b3f60d7e8ad90a9728b0ca75ec
Ahmed <ahmed15ayman7ahmed2002@gmail.com>
2024-08-08 13:48:56 +0300

    update auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth"
+import FacebookProvider from "next-auth/providers/facebook"
+import GoogleProvider from "next-auth/providers/google"
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+
+    }),
+    FacebookProvider({
+        clientId: process.env.FACEBOOK_CLIENT_ID!,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET!
+      })
+    
+  ],
+  secret:process.env.SECRET
+}
+
+export default NextAuth(authOptions)

8bb3028961eece88deca5f6af916decf966ea207
Ahmed <ahmed15ayman7ahmed2002@gmail.com>
2024-10-29 19:33:39 +0300

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,39 +11,39 @@ import "react-toastify/dist/ReactToastify.css";
 import InfintyProvider from "@/components/providers/InfintyProvider";
 import ReduxProvider from "@/components/providers/ReduxProvider";
 import Head from "next/head";
-export const metadata: Metadata = {
-  title: {
-    default:
-      "SPORTON | Home - Player Posts, Achievements, and Contact Players ",
-    template: "%s - SPORTON ",
-  },
-  keywords: [
-    "SPORTON",
-    "Home",
-    "Player Posts",
-    "Achievements",
-    "Contact Players",
-    "Showcase Talent",
-    "Connect with Clubs",
-    "Gain Recognition",
-    "Sports Platform",
-  ],
-  description:
-    "SPORTON is a sports community platform that includes all athletes from Egypt in various sports, and on the other hand, player agents and clubs and institutions that will receive their talents will be present.",
-  openGraph: {
-    type: "website",
-    url: "https://www.sporton.website/",
-    title: "SPORTON",
-    description:
-      "SPORTON is a sports community platform that includes all athletes from Egypt in various sports, and on the other hand, player agents and clubs and institutions that will receive their talents will be present.",
-    images: [
-      {
-        url: "https://www.sporton.website/logo.png",
-        alt: "SPORTON LOGO",
-      },
-    ],
-  },
-};
+// export const metadata: Metadata = {
+//   title: {
+//     default:
+//       "SPORTON | Home - Player Posts, Achievements, and Contact Players ",
+//     template: "%s - SPORTON ",
+//   },
+//   keywords: [
+//     "SPORTON",
+//     "Home",
+//     "Player Posts",
+//     "Achievements",
+//     "Contact Players",
+//     "Showcase Talent",
+//     "Connect with Clubs",
+//     "Gain Recognition",
+//     "Sports Platform",
+//   ],
+//   description:
+//     "SPORTON is a sports community platform that includes all athletes from Egypt in various sports, and on the other hand, player agents and clubs and institutions that will receive their talents will be present.",
+//   openGraph: {
+//     type: "website",
+//     url: "https://www.sporton.website/",
+//     title: "SPORTON",
+//     description:
+//       "SPORTON is a sports community platform that includes all athletes from Egypt in various sports, and on the other hand, player agents and clubs and institutions that will receive their talents will be present.",
+//     images: [
+//       {
+//         url: "https://www.sporton.website/logo.png",
+//         alt: "SPORTON LOGO",
+//       },
+//     ],
+//   },
+// };
 export default function RootLayout({
   children,
 }: {
